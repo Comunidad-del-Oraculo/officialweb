a6d48b090b49434160a155250719e294452ddece
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-12-20 18:59:26 -0300

    update layout and landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,17 @@
-import { Navbar } from "./components/Navbar";
 import Hero from "./components/Hero";
-import { DeveloperList } from "./components/DeveloperList";
+import { Developers } from "./components/Developers";
 import { AboutUs } from "./components/AboutUs";
-import { ActivitiesList } from "./components/ActivitiesList";
-import { Footer } from "./components/Footer";
+import { Activities } from "./components/Activities";
+import { ContactUs } from "./components/ContactUs";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <div id="hero">
-        <Hero />
-      </div>
-      <div id="developerlist">
-        <DeveloperList />
-      </div>
-      <div id="aboutus">
-        <AboutUs />
-      </div>
-      <div id="activitieslist">
-        <ActivitiesList />
-      </div>
-      <div id="contactanos">
-        <ActivitiesList />
-      </div>
-      <div id="footer">
-        <Footer />
-      </div>
-    </>
+    <div className="flex flex-col w-full">
+      <Hero />
+      <AboutUs />
+      <Developers />
+      <Activities />
+      <ContactUs />
+    </div>
   );
 }
