fecfd71f4f98709d39192005eb2f71479f1dc03b
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-12-23 09:21:23 -0300

    base config for run on local

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/.",
-  assetPrefix: "/.",
-
   images: {
     remotePatterns: [
       {
